99260977bce81afc881d03998c201243039d2418
ScriptKid <scriptkidhacks@gmail.com>
2021-05-23 18:31:19 +0200

    temporary fix lavawater not working in sp

--- src/main/java/io/gitlab/scriptkid/ezcheatclient/punishments/WaterLava/mixin/WaterLava_LavaFluidMixin.java
+++ src/main/java/io/gitlab/scriptkid/ezcheatclient/punishments/WaterLava/mixin/WaterLava_LavaFluidMixin.java
@@ -1,6 +1,7 @@
 package io.gitlab.scriptkid.ezcheatclient.punishments.WaterLava.mixin;
 
 import io.gitlab.scriptkid.ezcheatclient.FakeCheat;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.fluid.LavaFluid;
@@ -18,14 +19,18 @@ public class WaterLava_LavaFluidMixin {
     @Inject(at = @At("HEAD"), method = "getStill()Lnet/minecraft/fluid/Fluid;", cancellable = true)
     private void getStill(CallbackInfoReturnable<FlowableFluid> info) {
         if(FakeCheat.isActivated) {
-            info.setReturnValue(Fluids.WATER);
+            if(MinecraftClient.getInstance() != null && !MinecraftClient.getInstance().isMultiplayerEnabled()) {
+                info.setReturnValue(Fluids.WATER);
+            }
         }
     }
 
     @Inject(at = @At("HEAD"), method = "getFlowing()Lnet/minecraft/fluid/Fluid;", cancellable = true)
     private void getFlowing(CallbackInfoReturnable<FlowableFluid> info) {
         if(FakeCheat.isActivated) {
-            info.setReturnValue(Fluids.FLOWING_WATER);
+            if(MinecraftClient.getInstance() != null && !MinecraftClient.getInstance().isMultiplayerEnabled()) {
+                info.setReturnValue(Fluids.FLOWING_WATER);
+            }
         }
     }
 
